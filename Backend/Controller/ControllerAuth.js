7363ca961171878a234b0ed16e30f68b410eafcb
MarcusPersson97 <marcus.persson.97@hotmail.com>
2026-05-07 21:58:07 +0200

    Added a POST register user route

---
+++ Backend/Controller/ControllerAuth.js
@@ -0,0 +1,36 @@
+const authModel = require('../Model/ModelAuth');
+const bcrypt = require('bcrypt');
+const saltrounds = 10;
+
+async function register(req, res){
+
+    const {username, password, email, } = req.body;
+
+    try {
+        if(!username || !password || !email){
+
+           return res.status(400).json({message: "Bad request, username, password and email fields are required"});
+
+        }
+
+        const hashedPassword = bcrypt.hash(password, saltrounds);
+
+        const user = {
+            username,
+            hashedPassword,
+            email
+        }
+        
+        newUser = await authModel.register(user);
+        return res.status(201).json({message: "user was successfully registered", newUser});
+    } 
+    
+    catch (error) {
+        res.status(500).json({message: "Server error"});
+    }
+
+    
+}
+
+
+module.exports = {register};
