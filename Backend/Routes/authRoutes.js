b454f4ad61a305655cd29c179dae12dc2d065382
MarcusPersson97 <marcus.persson.97@hotmail.com>
2026-05-11 11:46:43 +0200

    removing a test function from authroutes

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -3,5 +3,4 @@ const router = express.Router();
 const authController = require('../Controller/ControllerAuth');
 
 router.post('/register', authController.register);
-router.get('/', authController.test)
 module.exports = router;
