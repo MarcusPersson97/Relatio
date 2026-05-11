b849fd5215109afc8046123d5f44bdf6c0b6d3f0
MarcusPersson97 <marcus.persson.97@hotmail.com>
2026-05-11 10:40:58 +0200

    Added a POST registration route to be able to make an account

--- Backend/Model/ModelAuth.js
+++ Backend/Model/ModelAuth.js
@@ -1,9 +1,15 @@
 const mysql = require('mysql2/promise');
+const db = require('../database');
 
 async function register(user){
 
 
-    
+const connection = db.Connect();
+
+const [rows] = await connection.execute('INSERT INTO railway.users (Username, Password, Email) VALUES (?, ?, ?)', 
+    [user.username, user.hashedPassword, email]);
+
+
 
 
 }
