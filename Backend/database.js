8900d4510a9f24bf524d4e2730870a596ad8b9ca
TeoTurland <teo@turland.se>
2026-05-11 14:07:08 +0200

    fix conections db

--- Backend/database.js
+++ Backend/database.js
@@ -3,20 +3,30 @@ const dotenv = require('dotenv')
 
 dotenv.config();
 
+async function Connect(){
+    try {
     const connection =  mysql.createPool({
-            database: process.env.DB,
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            port: process.env.PORT,
-            waitForConnections: true,
-            connectionLimit: 10,
-            queueLimit: 0,
-            enableKeepAlive: true,
-            keepAliveInitialDelay: 0
-        });
+                database: process.env.DB,
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                port: process.env.PORT,
+                waitForConnections: true,
+                connectionLimit: 10,
+                queueLimit: 0,
+                enableKeepAlive: true,
+                keepAliveInitialDelay: 0
+            });
 
 
+            return connection;
+
+
+    } catch(error){
+        console.error(`error: ${error.message}`)
+    }
+}
+   
 
 
 process.on('SIGINT', async () => {
@@ -24,4 +34,4 @@ process.on('SIGINT', async () => {
 } )
 
 
-module.exports = connection;
+module.exports = {Connect};
