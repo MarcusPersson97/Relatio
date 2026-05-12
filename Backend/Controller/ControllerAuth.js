cc169c61dd2b7dac6ce92dc744297ed3254ba68a
MarcusPersson97 <marcus.persson.97@hotmail.com>
2026-05-12 11:50:09 +0200

    Added a Login system, and some additional model db functions that aid retrieving data, like getUser, userExists and login

--- Backend/Controller/ControllerAuth.js
+++ Backend/Controller/ControllerAuth.js
@@ -1,5 +1,6 @@
 const authModel = require('../Model/ModelAuth');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const saltrounds = 10;
 
 async function register(req, res){
@@ -20,7 +21,7 @@ async function register(req, res){
             hashedPassword,
             email
         }
-        
+
         const newUser = await authModel.register(user);
         return res.status(201).json({message: "user was successfully registered", newUser});
     } 
@@ -32,7 +33,39 @@ async function register(req, res){
     
 }
 
+async function login(req, res){
+
+    const {username, password} = req.body;
+    
+    if(!username || !password){
+       return res.status(400).json({message: "bad request, incorrect data"});
+
+    }
+
+    try {
+        const storedUser = await authModel.userExists(username);
+        if(!storedUser){
+            return res.status(404).json({message: "user was not found"});
+        }
+
+        const isValidPassword = await bcrypt.compare(password, storedUser.password);
+
+        if(isValidPassword){
+            const userInfo = await authModel.login(username);
+            return res.status(200).json({message: "You successfully logged in", userInfo});
+        }
+        else{
+            return res.status(401).json({message: "incorrect login credentials"});        
+        }
+    } 
+
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({message: "Server error"});
+    }
+
+}
 
 
 
-module.exports = {register};
+module.exports = {register, login};
--- Backend/Model/ModelAuth.js
+++ Backend/Model/ModelAuth.js
@@ -2,16 +2,52 @@ const mysql = require('mysql2/promise');
 const db = require('../database');
 
 async function register(user){
-
-
 const connection = await db.Connect();
 
 const [rows] = await connection.query('INSERT INTO railway.users (Username, Password, Email) VALUES (?, ?, ?)', 
     [user.username, user.hashedPassword, user.email]);
 
     return rows;
-    
+
 
 }
 
-module.exports = {register};
+async function login(username){
+
+const [rows] = await connection.query('SELECT username, email FROM railway.users WHERE username = ?', [username]);
+return rows[0];
+
+}
+
+async function userExists(username){
+
+const [rows] = await connection.query('SELECT password FROM railway.users WHERE username = ?', [username]);
+if(rows.length>0){
+
+    return rows[0];
+
+}
+else{
+    
+    return null;
+
+}
+}
+
+async function getUser(username){
+
+const [rows] = await connection.query('SELECT * FROM railway.users WHERE username = ?', [username]);
+if(rows.length>0){
+
+    return rows[0];
+
+}
+else{
+
+    return null;
+
+}
+}
+
+
+module.exports = {register, login, getUser, userExists};
