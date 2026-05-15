416cb7357d59c0fc06cdbdad44d81a665d97b8ba
TeoTurland <teo@turland.se>
2026-05-15 10:28:20 +0200

    add cors, await in controllerAuth

--- Backend/Controller/ControllerAuth.js
+++ Backend/Controller/ControllerAuth.js
@@ -61,7 +61,7 @@ async function login(req, res){
         const isValidPassword = await bcrypt.compare(password, storedUser.Password);
 
         if(isValidPassword){
-            const token = authServices.createJwt(storedUser);
+            const token = await authServices.createJwt(storedUser);
             const userInfo = await authModel.login(email);
             return res.status(200).json({message: "You successfully logged in", userInfo, token});
         }
--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors()); 
+
 const authRoutes = require('./Routes/authRoutes');
 app.use(express.json());
 
